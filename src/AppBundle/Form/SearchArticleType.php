e24ef6678c7ae8cfc850d600d9e4c510d6fa5de6
Margaux SMITS <smits.margaux@gmail.com>
2017-01-10 16:07:36 +0100

    Form to search posts by tags or by name

---
+++ src/AppBundle/Form/SearchArticleType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
+
+class SearchArticleType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+          ->add('name')
+          ->add('tag');
+    }
+
+
+}
