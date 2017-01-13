8bde8264d4685f60bb207f7c6f25e1619b99294d
Margaux Smits <smits.margaux@gmail.com>
2017-01-13 07:55:20 +0100

    Design

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new Vich\UploaderBundle\VichUploaderBundle(),
             new Ivory\CKEditorBundle\IvoryCKEditorBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
+
   ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
